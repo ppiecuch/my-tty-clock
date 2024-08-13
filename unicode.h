754652e79f1a7080f8c22dcc70df118c0322cb40
Pablo P. <piecuch.pawel@gmail.com>
2024-08-13 21:31:35 +0200

    Update unicode.h

--- unicode.h
+++ unicode.h
@@ -1,15 +1,12 @@
-
+/*
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ *  Copyright (C) 2024-2024  The DOSBox Staging Team
+ */
 
 // Specifies how to interpret characters 0x00-0x1f and 0x7f in DOS strings
 enum class DosStringConvertMode {
-
-	// String contains control codes (new line, tabulation, delete, etc.)
-	WithControlCodes,
-
-	// String does not have any codes characters, consider all characters
-	// as screen codes
-	ScreenCodesOnly,
-
-	// String should not contain characters mentioned above
-	NoSpecialCharacters
+	WithControlCodes, // String contains control codes (new line, tabulation, delete, etc.)
+	ScreenCodesOnly, // String does not have any codes characters, consider all characters as screen codes
+	NoSpecialCharacters, // String should not contain characters mentioned above
 }
