4694f53940466171f9dbfc839e755ca5e7e870fe
Pawel Piecuch <piecuch.pawel@gmail.com>
2024-08-13 21:19:16 +0200

    initial import of application

---
+++ README.md
@@ -0,0 +1,8 @@
+# my-tty-clock
+
+`tty-clock` with my personal improvments and extensions.
+
+
+### Reference:
+
+1. https://github.com/xorg62/tty-clock
---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+if [[ $(uname -o) == "Darwin" ]]; then
+  CURL="-framework Foundation -framework SystemConfiguration"
+  for lib in curl psl ssl crypto nghttp2 idn2 unistring brotlidec-static brotlicommon-static iconv zstd z; do
+    CURL="$CURL /opt/local/lib/lib$lib.a"
+  done
+else
+  CURL="-lcurl"
+fi
+
+LIBS="$(ncurses6-config --libs)"
+OPTS="-fpermissive"
+
+g++ -o my-tty-clock $OPTS -I figlet main.cpp figlet/*.cpp $CURL $LIBS
