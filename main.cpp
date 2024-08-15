93e5546d6ceba3b75ac01a0aa9e6cd726013fed8
Pawel Piecuch <piecuch.pawel@gmail.com>
2024-08-15 23:15:16 +0200

    update status

--- main.cpp
+++ main.cpp
@@ -197,7 +197,7 @@ int main(int argc, char *argv[]) {
 			const char *sect = sections.begin()->pItem; // first section
 			int key = rand() % ini.GetSectionSize(sect);
 
-			std::string s = ini.GetValue(sect, f_ssprintf("%s", key));
+			std::string s = ini.GetValue(sect, f_ssprintf("%d", key));
 			std::string delimiter = "::";
 			line1 = s.substr(0, s.find(delimiter));
 			line2 = s.substr(s.find(delimiter) + 2);
