5e5eb8759f85577789b77278e1bed4e1c0ca776a
Pawel Piecuch <piecuch.pawel@gmail.com>
2024-08-31 15:42:37 +0200

    cron implementation

--- maincron.cpp
+++ maincron.cpp
@@ -1,9 +1,11 @@
 #define _X_OPEN_SOURCE_EXTENDED
 
+#include <spawn.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 #include <time.h>
 #include <unistd.h>
 
@@ -11,12 +13,14 @@
 #include "par_easycurl.h"
 #include "simpleini/SimpleIni.h"
 
+#include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
 
-#define APPVERSION "0.3"
-#define PRINTCMD "./my-word-memo -p -1"
+#define APPVERSION "0.4"
+#define APPNAME "main(v" APPVERSION ")"
+#define PRINTCMD "my-word-memo -p -1"
 
 #define f_ssprintf(...) \
 	({ int _ss_size = snprintf(0, 0, ##__VA_ARGS__);    \
@@ -30,6 +34,48 @@ using namespace datetime_utils::crontab;
 // ----------
 // - https://github.com/peychart/croncpp/blob/main/main.cpp
 // - format of a cron string : "S M H d m w [Y] cmd" - (Year is optional; default limit values of the year: +/- 8 years relative to the current year).
+
+extern char **environ;
+
+int run_cmd(const char *cmd, const char *args) {
+	pid_t pid;
+	int status = posix_spawnp(&pid, cmd, nullptr, nullptr, args, environ);
+	if (status == 0) {
+		if (waitpid(pid, &status, 0) != -1) {
+			if (WIFEXITED(status)) {
+				return WEXITSTATUS(status);
+			} else {
+				if (WIFSIGNALED(status)) {
+					if (WCOREDUMP(status)) {
+						fprintf(stderr, APPNAME ": the child process produced a core dump\n");
+					}
+					if (WTERMSIG(status)) {
+						fprintf(stderr, APPNAME ": the child process was terminated\n");
+					}
+				}
+			}
+		} else {
+			fprintf(stderr, APPNAME ": waitpid error\n");
+		}
+	} else {
+		fprintf(APPNAME ": posix_spawn: %s\n", strerror(status));
+	}
+	return -1;
+}
+
+std::vector<std::string> read_file(const char *filename) {
+	std::vector<std::string> r;
+	std::ifstream file(filename);
+	if (file.is_open()) {
+		std::string line;
+		while (std::getline(file, line)) {
+			if (!line.empty())
+				r.push_back(line);
+		}
+		file.close();
+	}
+	return r;
+}
 
 int main(int argc, char **argv) {
 	std::vector<std::string> crontab = {
@@ -52,7 +98,8 @@ int main(int argc, char **argv) {
 			strftime(buffer, 80, "%Y/%m/%d %H:%M:%S", localtime(&rawtime));
 			int schedule = rawtime - Now;
 			std::cout << "The job \"" << c.expression() << "\" lanched at: " << rawtime << " (" << buffer << "), in " << schedule << " sec. - \"" << crontab[i] << "\"" << std::endl;
-			if (schedule < 0) {
+			if (schedule >= -1 && schedule <= 1) {
+				// run
 			} else if (!pause || schedule < pause) {
 				pause = schedule;
 			}
