068d544270fd69c9a244767a5690199d5ec7ad99
Pawel Piecuch <piecuch.pawel@gmail.com>
2024-10-08 14:14:04 +0200

    update mp3 player

--- modules/gtts/mp3.h
+++ modules/gtts/mp3.h
@@ -11,17 +11,36 @@
 #include <alsaplayer/control.h>
 #include <stdio.h>
 
+#include <string>
+
+template <class T>
+std::string get(int (*f)(int, T *), int sid) {
+	T rval;
+	int err = f(sid, &rval);
+	if (0 == err)
+		return std::string("<err>");
+	return std::to_string(rval);
+}
+
+template <int MaxSize>
+std::string get_string(int (*f)(int, char *), int sid) {
+	char result[MaxSize + 1];
+	int err = f(sid, result);
+	if (0 == err)
+		return std::string("<err>");
+	return std::string(result);
+}
+
 struct mad_player_t {
 	FILE *log;
 	int session = 0;
 
 	mad_player_t(FILE *f = stderr) :
 			log(f) {
-		fprintf(f, "Player created (ver. %d):", ap_version());
-		auto get_volume = [=]() { float v; ap_get_volume(session, &v); return v; };
-		fprintf(f, "  volume: %0.2f\n", get_volume());
-		auto get_speed = [=]() { float v; ap_get_speed(session, &v); return v; };
-		fprintf(f, "  speed: %0.2f\n", get_speed());
+		fprintf(f, "Player created (ver. %d):\n", ap_version());
+		fprintf(f, "  status: %s\n", get_string<AP_STATUS_MAX>(ap_get_status, sid).c_str());
+		fprintf(f, "  volume: %s\n", get<float>(&ap_get_volume, session).c_str());
+		fprintf(f, "  speed: %s\n", get<float>(&ap_get_speed, session).c_str());
 	}
 
 	~mad_player_t() {
